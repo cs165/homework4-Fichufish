5ce085e58f87abbc6d9d107273314c6e60773433
Fichufish <39399950+Fichufish@users.noreply.github.com>
2019-05-16 20:57:44 +0800

    Update menu-screen.js

--- menu-screen.js
+++ menu-screen.js
@@ -5,6 +5,28 @@
 class MenuScreen {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
+    const song = new Song();
   }
   // TODO(you): Add methods as necessary.
+}
+
+class Song {
+  constructor() {
+    const JSON_PATH = 'https://fullstackccu.github.io/homeworks/hw4/songs.json';
+    fetch(JSON_PATH)
+      .then(this._onResponse)
+      .then(this._onJsonReady);
+
+    //this._onJsonReady = this._onJsonReady.bind(this);
+  }
+
+  _onJsonReady(json) {
+    //this.songInfo = json.songs;
+    console.log(json);
+  }
+
+  _onResponse(response) {
+    return response.json();
+  }
+
 }
