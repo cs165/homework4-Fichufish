377b3c36afad733063de28b01bf881d938c2e056
Fichufish <39399950+Fichufish@users.noreply.github.com>
2019-05-18 15:04:31 +0800

    Update menu-screen.js

--- menu-screen.js
+++ menu-screen.js
@@ -5,33 +5,35 @@
 class MenuScreen {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
-    const song = new Song();
+    this.containerElement = document.querySelector('select#song-selector');
+    const song = new Song(this.containerElement);
   }
   // TODO(you): Add methods as necessary.
 }
 
 class Song {
-  constructor() {
+  constructor(containerElement) {
+    this.containerElement = containerElement;
     this.songInfo = {};
     
     //this._loadSongs = this._loadSongs.bind(this);
 
-    this._loadSongs();
+    this._loadSongs(this.containerElement);
   }
 
-  _loadSongs() {
+  _loadSongs(containerElement) {
     const JSON_PATH = 'https://fullstackccu.github.io/homeworks/hw4/songs.json';
     const onJsonReady = (json) => {
       this.songInfo = json;
-      this._createSongs();
+      this._createSongs(containerElement);
     };
     fetch(JSON_PATH)
       .then(reponse => reponse.json())
       .then(onJsonReady);
   }
 
-  _createSongs() {
-    console.log(this.songInfo);
+  _createSongs(containerElement) {
+    console.log(containerElement)
     const songSelector = document.querySelector('select#song-selector');
     const SONGS = Object.values(this.songInfo);
     for(let i=0; i<SONGS.length; i++) {
