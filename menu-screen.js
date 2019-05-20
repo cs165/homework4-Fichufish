0af33912f633cd51860983c06792d93bac6a0192
Fichufish <39399950+Fichufish@users.noreply.github.com>
2019-05-20 20:03:49 +0800

    Omo)/ second complete

--- menu-screen.js
+++ menu-screen.js
@@ -66,6 +66,7 @@ class Song {
 
   _loadSongs(containerElement) {
     const JSON_PATH = 'https://fullstackccu.github.io/homeworks/hw4/songs.json';
+    
     const onJsonReady = (json) => {
       this.songInfo = json;
       this._createSongs(containerElement);
